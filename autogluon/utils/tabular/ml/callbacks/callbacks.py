9570f87af45482e35004344834189cb7571cf171
Alexander Shirkov <sgdread@gmail.com>
2020-04-20 01:24:16 -0700

    Added callbacks support docs.

--- autogluon/utils/tabular/ml/callbacks/callbacks.py
+++ autogluon/utils/tabular/ml/callbacks/callbacks.py
@@ -23,17 +23,41 @@ class CallbackList:
 
 
 class CallbackManager:
+    """
+    Callbacks Manager transparently operating on non-wired callbacks.
+    Contract: Callbacks are processed as a filter Chain::
+        (ArgA, ArgB) -> callbackA -> callbackB -> ... -> (ArgA', ArgB')
+    methods must return updated values in the same order as in method signature.
+
+    """
 
     def __init__(self):
         self.callbacks: Dict[str, list] = {}
 
-    def register_callback(self, operation, callback, index=None):
-        op_callbacks: list = self.callbacks.get(operation, list())
+    def register_callback(self, namespace: str, callback: object, index: int = None):
+        """
+        Registers callback for operaton. Callback later can be called::
+
+            argA, argB = callback.namespace.method(argA, argB)
+
+        if there are no callbacks registered, all function calls will act transparently and return same values, passed into the function.
+
+        Parameters
+        ----------
+        namespace: str
+            operations namespace
+        callback: obj
+            callback instance implementing callback methods.
+        index: int (optional)
+            position what position to add the callback - use this to inject high-priority preprocessing.
+            Defaults to None - this will add new callback at the end of the filter chain.
+        """
+        op_callbacks: list = self.callbacks.get(namespace, list())
         if index is None:
             op_callbacks.append(callback)
         else:
             op_callbacks.insert(index, callback)
-        self.callbacks[operation] = op_callbacks
+        self.callbacks[namespace] = op_callbacks
 
     def __getattr__(self, operation):
         if operation.startswith('__'):
